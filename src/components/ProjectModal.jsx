1a6b4e820aa5b7d2a613b5fa7ba75f0c4da8c497
Antoine Paturel <antoine.paturel34@free.fr>
2023-08-20 14:04:09 +0200

    fix carousel

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -6,29 +6,20 @@ import ReactMarkdown from 'react-markdown'
 const ProjectModal = (props) => {
   const { project } = props
   const { attributes } = project;
-  const imgUrl = (nb) => {
-    return (
-      attributes.screens.data[nb].attributes.url || ''
-    )
-  }
-  //const htmlDescription = marked(attributes.description);
+
+  const generateCarouselItems = () => {
+    return attributes.screens.data.map((screen, index) => (
+      <div key={index}>
+        <img className='carousel-img' src={screen.attributes.url} alt={`Image ${index + 1}`} />
+      </div>
+    ));
+  };
 
   return (
     <div className="modal-content">
       <br />
       <Carousel width={'80%'} infiniteLoop={true}>
-        <div>
-          <img className='carousel-img' src={`${imgUrl(0)}`} />
-        </div>
-        <div>
-          <img className='carousel-img' src={`${imgUrl(1)}`} />
-        </div>
-        <div>
-          <img className='carousel-img' src={`${imgUrl(2)}`} />
-        </div>
-        <div>
-          <img className='carousel-img' src={`${imgUrl(3)}`} />
-        </div>
+        {generateCarouselItems()}
       </Carousel>
       <h1 className='custom-title'>{attributes.title}</h1>
       <ReactMarkdown>{attributes.description}</ReactMarkdown>
@@ -36,4 +27,4 @@ const ProjectModal = (props) => {
   );
 };
 
-export default ProjectModal
+export default ProjectModal;
